17fd4feeaba76e3ae897508eb24d3b29be3eeee7
Abhishek <abhishekdwi.05@gmail.com>
2021-01-05 15:07:54 -0800

    GET API to fetch videos based on upload time

--- youtube/api/views.py
+++ youtube/api/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello")
+    video_list = models.Videos.objects.order_by('-published_date')
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(video_list, 10)
+
+    try:
+        videos = paginator.page(page)
+    except PageNotAnInteger:
+        videos = paginator.page(1)
+    except EmptyPage:
+        videos = paginator.page(paginator.num_pages)
+
+    return render(request, 'video_display.html', {'videos':videos})
